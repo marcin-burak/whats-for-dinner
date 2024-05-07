71075135e0666e32b495f6fd655e1bfab638d6ae
Marcin Burak <marcin.burak@outlook.com>
2024-05-07 18:38:11 +0200

    Add Open API options validation tests.

---
+++ test/WhatsForDinner.Web.Tests/Dependencies/OpenApi/ConfigurationTests.cs
@@ -0,0 +1,88 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.Extensions.Hosting;
+using NSubstitute;
+using WhatsForDinner.Web.Dependencies.OpenApi;
+
+namespace WhatsForDinner.Web.Tests.Dependencies.OpenApi;
+
+public sealed class ConfigurationTests
+{
+    [Theory]
+    [MemberData(nameof(ValidOptions))]
+    public void ValidationSucceeds(OpenApiOptions options, IHostEnvironment environment)
+    {
+        OpenApiOptionsValidator validator = new(environment);
+
+        var result = validator.TestValidate(options);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidOptions))]
+    public void ValidationFails(OpenApiOptions options, IHostEnvironment environment)
+    {
+        OpenApiOptionsValidator validator = new(environment);
+
+        var result = validator.TestValidate(options);
+
+        result.ShouldHaveAnyValidationError();
+    }
+
+
+
+    #region Test data
+
+    private static IHostEnvironment DevelopmentEnvironment
+    {
+        get
+        {
+            var mock = Substitute.For<IHostEnvironment>();
+            mock.EnvironmentName = "Development";
+
+            return mock;
+        }
+    }
+
+    private static IHostEnvironment NonDevelopmentEnvironment
+    {
+        get
+        {
+            var mock = Substitute.For<IHostEnvironment>();
+            mock.EnvironmentName = "NonDevelopment";
+
+            return mock;
+        }
+    }
+
+    public static TheoryData<OpenApiOptions, IHostEnvironment> ValidOptions => new()
+    {
+        {
+            new()
+            {
+                Enabled = true
+            },
+            DevelopmentEnvironment
+        },
+        {
+            new()
+            {
+                Enabled = false
+            },
+            NonDevelopmentEnvironment
+        },
+    };
+
+    public static TheoryData<OpenApiOptions, IHostEnvironment> InvalidOptions => new()
+    {
+        {
+            new()
+            {
+                Enabled = true
+            },
+            NonDevelopmentEnvironment
+        },
+    };
+
+    #endregion
+}
